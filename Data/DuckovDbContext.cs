e2d713e2875c6149a43ec7c45da2dace6a8fb4b7
Emre <emrekaray@gmail.com>
2026-01-07 18:57:48 +0100

    Added DbContext with populated database

---
+++ Data/DuckovDbContext.cs
@@ -0,0 +1,21 @@
+using DuckovItemsApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DuckovItemsApi.Data;
+
+public class DuckovDbContext : DbContext
+{
+    public DbSet<Item> Items { get; set; }
+    public DbSet<ItemSpawn> ItemSpawns { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Container> Containers { get; set; }
+    public DbSet<Map> Maps { get; set; }
+
+    public DuckovDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected DuckovDbContext()
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
+using DuckovItemsApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DuckovDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+    if (builder.Environment.IsDevelopment())
+    {
+        var folderPath = Path.Combine(builder.Environment.ContentRootPath, "App_Data");
+        Directory.CreateDirectory(folderPath); // Ensure folder exists
+    }
+
+    options.UseSqlite(connectionString);
+});
 
 var app = builder.Build();
 
@@ -12,6 +28,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DuckovDbContext>();
+    db.Database.EnsureCreated();
 }
 
 app.UseHttpsRedirection();
